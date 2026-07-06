1a7bde58091c0a2465c4b92cb1654e97200d5053
vox403 <java960419@gmail.com>
2026-07-07 02:46:16 +0900

    Delete js directory

--- js/view.js
+++
@@ -1,6 +0,0 @@
-export { bindViewEvents } from "./ui/events.js";
-export { readInvestAmount, writeInvestAmount } from "./ui/orders.js";
-export { renderLeaderboard } from "./ui/leaderboard.js";
-export { renderMarket } from "./ui/market.js";
-export { renderProfile, setPlayerName } from "./ui/profile.js";
-export { readExchangeAmount, setBusyView, setLog, showActive, showLocked, writeExchangeAmount } from "./ui/shell.js";
